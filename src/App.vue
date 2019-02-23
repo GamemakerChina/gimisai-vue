ba699775ac70b99bf487c13f319dd9a48ac72270
liaronce <qq1062787843@hotmail.com>
2019-02-23 17:11:25 +0800

    Init

---
+++ src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <div id="app">
+    <mu-appbar title="GiMiSai" class="gimisai-appbar" color="primary" :class="{'nav-hide': !open}">
+        <mu-button icon slot="left" @click="toggle()"><mu-icon value="menu"></mu-icon></mu-button>
+        <mu-button flat slot="right">_</mu-button>
+        <mu-button flat slot="right">X</mu-button>
+    </mu-appbar>
+    <sidebar :show="open" v-on:changeState="toggle"></sidebar>
+  </div>
+</template>
+
+<script>
+import Sidebar from './components/GiMiSai-sidebar'
+export default {
+  name: 'App',
+  data(){
+    return {
+        open: true
+    }
+  },
+  components: {
+    Sidebar
+  },
+  methods: {
+    toggle(msg){
+      console.log(msg);
+      this.open = !this.open
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
+.gimisai-appbar {
+  position: fixed;
+  left: 256px;
+  right: 0;
+  top: 0;
+  width: auto;
+  transition: all .45s;
+}
+.gimisai-appbar.nav-hide {
+  left: 0;
+}
+</style>
---
+++ src/components/GiMiSai-sidebar.vue
@@ -0,0 +1,75 @@
+<template>
+    <section>
+        <div class="sidebar">
+            <mu-drawer :open="show">
+                <mu-list>
+                    <mu-list-item button :ripple="false">
+                        <mu-list-item-action>
+                            <mu-icon value="inbox"></mu-icon>
+                        </mu-list-item-action>
+                        <mu-list-item-title>首页</mu-list-item-title>
+                    </mu-list-item>
+                    <mu-list-item button :ripple="false">
+                        <mu-list-item-action>
+                            <mu-icon value="grade"></mu-icon>
+                        </mu-list-item-action>
+                        <mu-list-item-title>GameMaker Studio 2</mu-list-item-title>
+                    </mu-list-item>
+                    <mu-list-item button :ripple="false">
+                        <mu-list-item-action>
+                            <mu-icon value="send"></mu-icon>
+                        </mu-list-item-action>
+                        <mu-list-item-title>GameMaker: Studio</mu-list-item-title>
+                    </mu-list-item>
+                    <mu-list-item button :ripple="false">
+                        <mu-list-item-action>
+                            <mu-icon value="drafts"></mu-icon>
+                        </mu-list-item-action>
+                        <mu-list-item-title>GameMaker 7/8/8.1</mu-list-item-title>
+                    </mu-list-item>
+                    <mu-list-item button :ripple="false">
+                        <mu-list-item-action>
+                            <mu-icon value="drafts"></mu-icon>
+                        </mu-list-item-action>
+                        <mu-list-item-title>LateralGM</mu-list-item-title>
+                    </mu-list-item>
+                    <mu-list-item button :ripple="false">
+                        <mu-list-item-action>
+                            <mu-icon value="drafts"></mu-icon>
+                        </mu-list-item-action>
+                        <mu-list-item-title>RadialGM</mu-list-item-title>
+                    </mu-list-item>
+                    <mu-list-item button :ripple="false">
+                        <mu-list-item-action>
+                            <mu-icon value="drafts"></mu-icon>
+                        </mu-list-item-action>
+                        <mu-list-item-title>论坛</mu-list-item-title>
+                    </mu-list-item>
+                    <mu-list-item button :ripple="false">
+                        <mu-list-item-action>
+                            <mu-icon value="drafts"></mu-icon>
+                        </mu-list-item-action>
+                        <mu-list-item-title>设置</mu-list-item-title>
+                    </mu-list-item>
+                    <mu-list-item button :ripple="false">
+                        <mu-list-item-action>
+                            <mu-icon value="drafts"></mu-icon>
+                        </mu-list-item-action>
+                        <mu-list-item-title>关于</mu-list-item-title>
+                    </mu-list-item>
+                </mu-list>
+            </mu-drawer>
+        </div>
+    </section>
+</template>
+
+<script>
+export default {
+    props: ['show'],
+    data () {
+        return {
+            open: true
+        };
+    }
+}
+</script>
