c4ea6f19d4deef9b02148d44686d1a8df577e90f
liaronce <qq1062787843@hotmail.com>
2019-02-25 16:46:11 +0800

    Rewrite

--- src/components/layout/GiMiSai-nav-gms.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <section>
-        <mu-tabs value="nav-sub-gms">
-            <mu-tab title="nav1" value="nav-sub-gms-nav1"></mu-tab>
-            <mu-tab title="nav2" value="nav-sub-gms-nav2"></mu-tab>
-        </mu-tabs>
-    </section>
-</template>
